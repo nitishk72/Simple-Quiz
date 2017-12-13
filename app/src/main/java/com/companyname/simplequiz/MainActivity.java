51ad76205b539fc38b70b23accb36f902dff4375
Nitish Kumar Singh <nitishk72@gmail.com>
2017-12-13 18:18:41 +0530

    Database Helper

--- app/src/main/java/com/companyname/simplequiz/MainActivity.java
+++ app/src/main/java/com/companyname/simplequiz/MainActivity.java
@@ -3,11 +3,16 @@ package com.companyname.simplequiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<Question> QuestionArrayList ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Question q ;
+
     }
 }
